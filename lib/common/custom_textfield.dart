b0ec6034a6bcd5846f043a0d99419c0ce8719c32
SoniaVeronicaShoucair <sonia.shoucair@gmail.com>
2023-05-16 17:59:07 +0300

    initial commit

--- lib/common/custom_textfield.dart
+++ lib/common/custom_textfield.dart
@@ -3,8 +3,13 @@ import 'package:flutter/material.dart';
 class CustomTextField extends StatelessWidget {
   final TextEditingController controller;
   final String hintText;
-  const CustomTextField({Key? key, required this.controller,required this.hintText,required this.maxLines}) : super(key: key);
   final int maxLines;
+  const CustomTextField({Key? key,
+    required this.controller,
+    required this.hintText,
+    this.maxLines=1,
+  }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return TextFormField(
@@ -28,6 +33,7 @@ class CustomTextField extends StatelessWidget {
         }
         return null;
       },
+      maxLines: maxLines,
     );
   }
 }
--- lib/features/admin/screens/add_product_screen.dart
+++ lib/features/admin/screens/add_product_screen.dart
@@ -1,8 +1,14 @@
+import 'dart:io';
+
+import 'package:carousel_slider/carousel_options.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 
+import '../../../common/custom_button.dart';
 import '../../../common/custom_textfield.dart';
 import '../../../constants/global_variables.dart';
+import '../../../constants/utils.dart';
 
 class AddProductScreen extends StatefulWidget {
   static const String routeName='/add-product';
@@ -18,6 +24,8 @@ class _AddProductScreenState extends State<AddProductScreen> {
   final TextEditingController priceController = TextEditingController();
   final TextEditingController quantityController = TextEditingController();
 
+  String category = 'Mobiles';
+  List<File> images = [];
   @override
   void dispose() {
     super.dispose();
@@ -26,6 +34,22 @@ class _AddProductScreenState extends State<AddProductScreen> {
     priceController.dispose();
     quantityController.dispose();
   }
+
+  List<String> productCategories = [
+    'Mobiles',
+    'Essentials',
+    'Appliances',
+    'Books',
+    'Fashion'
+  ];
+
+  void selectImages() async {
+    var res = await pickImages();
+    setState(() {
+      images = res;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold (
@@ -51,32 +75,51 @@ class _AddProductScreenState extends State<AddProductScreen> {
             child: Column(
               children: [
                 const SizedBox(height: 20),
-                DottedBorder(
-                  borderType: BorderType.RRect,
-                  radius: const Radius.circular(10),
-                  dashPattern:const [10,4],
-                  strokeCap: StrokeCap.round,
-                  child:Container(
-                  width: double.infinity,
-                  height: 150,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const Icon(Icons.folder_open,size: 40,),
-                      const SizedBox(height: 15),
-                      Text(
-                        'Select Product Images',
-                        style: TextStyle(
-                          fontSize: 15,
-                          color: Colors.grey.shade400,
+                images.isNotEmpty? CarouselSlider(
+          items: images.map(
+              (i) {
+        return Builder(
+        builder: (BuildContext context) => Image.file(
+        i,
+        fit: BoxFit.cover,
+        height: 200,
+        ),
+        );
+        },
+        ).toList(),
+        options: CarouselOptions(
+          viewportFraction: 1,
+          height: 200,
+        ),
+      ):GestureDetector(
+                  onTap: selectImages,
+                  child: DottedBorder(
+                    borderType: BorderType.RRect,
+                    radius: const Radius.circular(10),
+                    dashPattern:const [10,4],
+                    strokeCap: StrokeCap.round,
+                    child:Container(
+                    width: double.infinity,
+                    height: 150,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Icon(Icons.folder_open,size: 40,),
+                        const SizedBox(height: 15),
+                        Text(
+                          'Select Product Images',
+                          style: TextStyle(
+                            fontSize: 15,
+                            color: Colors.grey.shade400,
+                          ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
-                ),
+                  ),
                 ),
                 const SizedBox(height: 30),
                 CustomTextField(
@@ -89,6 +132,42 @@ class _AddProductScreenState extends State<AddProductScreen> {
                   controller: descriptionController,
                   hintText: 'Description',
                   maxLines: 7,
+                ),
+
+                const SizedBox(height: 10),
+                CustomTextField(
+                  controller: priceController,
+                  hintText: 'Price',
+                ),
+                const SizedBox(height: 10),
+                CustomTextField(
+                  controller: quantityController,
+                  hintText: 'Quantity',
+                ),
+
+                const SizedBox(height: 10),
+                SizedBox(
+                  width: double.infinity,
+                  child: DropdownButton(
+                    value: category,
+                    icon: const Icon(Icons.keyboard_arrow_down),
+                    items: productCategories.map((String item) {
+                      return DropdownMenuItem(
+                        value: item,
+                        child: Text(item),
+                      );
+                    }).toList(),
+                    onChanged: (String? newVal) {
+                      setState(() {
+                        category = newVal!;
+                      });
+                    },
+                  ),
+                ),
+                const SizedBox(height: 10),
+                CustomButton(
+                  text: 'Sell',
+                  onTap: sellProduct,
                 ),
               ],
             ),
