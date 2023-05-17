820f70f9ee8774233a2da4784a15f4b41b145054
lea-noun <201910680@ua.edu.lb>
2023-05-17 18:21:16 +0300

    change ip

--- lib/constants/global_variables.dart
+++ lib/constants/global_variables.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
-String uri='http://192.168.1.66:3000';
+String uri='http://172.16.186.102:3000';
+
 class GlobalVariables{
   // COLORS
   static const appBarGradient = LinearGradient(
